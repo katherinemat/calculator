fd2a4158ec1a6faed450e322d1139bacd977b08b
Elise St Hilaire and Kaz Matthews <elise@>
2017-01-11 11:57:55 -0800

    make output div and print the results of the add function to that div.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,33 @@
+
  //For Addition
 var add = function(number1, number2) {
 	return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var addResult = add(number1, number2);
-
-alert(addResult);
 
 // For Subtraction
 var sub = function(number1, number2) {
 	return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var subResult = sub(number1, number2);
-
-alert(subResult);
-
 // For Multiplication
 var mult = function(number1, number2) {
 	return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var multResult = mult(number1, number2);
-
-alert(multResult);
 
 // For Division
 var div = function(number1, number2) {
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var divResult = div(number1, number2);
 
-alert(divResult);
+$(document).ready(function() {
+
+	$("form#add").submit(function(event) {
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = (add(number1, number2));
+		$("#output").text(result);
+		event.preventDefault();
+	});
+
+});
